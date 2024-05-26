6e8c37dc93754b1187527590a5131d0ef6838fbc
eslam <eslammongy995@gmail.com>
2024-05-26 10:46:01 +0300

    fix uploading profile image issue

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,8 +1,7 @@
-const String dummyBio =
-    "Hi Johan, is there a repo anywhere that I can check my code against please. It all worked fine until I extracted the ProfileMenuItem code";
-const String dummyName = "Eslam Mongy";
-const String dummyEmail = "eslammongy880@gmail.com";
-const String dummyPhone = "01001111111";
+const String dummyBio = "none bio";
+const String dummyName = "none name";
+const String dummyEmail = "none email";
+const String dummyPhone = "none phone";
 const String dummyImageUrl =
     "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
 
